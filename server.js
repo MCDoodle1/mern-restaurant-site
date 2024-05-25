ae3a2f8ee258bb4cc19a898fd1643accfb6293e1
MCDoodle1 <mclarijs@me.com>
2024-05-26 00:21:56 +0200

    Prepare deploy to Render

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./database/db');
-const cors = require('cors');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser')
-const authRoutes = require('./routes/auth');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const filterRoutes = require('./routes/filter');
-const paymentRoutes = require('./routes/payment');
+const connectDB = require("./database/db");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const filterRoutes = require("./routes/filter");
+const paymentRoutes = require("./routes/payment");
+const path = require("path");
 
 // middleware
-app.use(cors({
-    origin: ["https://mern-restaurant-site.onrender.com"]
-}));
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', authRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/product', productRoutes);
-app.use('/api/filter', filterRoutes);
-app.use('/api/payment', paymentRoutes);
-app.use('/uploads', express.static('uploads'));
+app.use("/api/auth", authRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/filter", filterRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/uploads", express.static("uploads"));
+app.use(express.static(path.join("client/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join("client", "build", "index.html"));
+});
 
 connectDB();
 
